5d92c68a28f2d9fc75d96168973eb808a532b6d4
Andreas Ohlsson <andreas.ohlsson@applio.se>
2022-12-29 14:24:17 +0100

    Change in title

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'sjpi22 demo' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'detta är sjpi22 demo-app' });
 });
 
 module.exports = router;
